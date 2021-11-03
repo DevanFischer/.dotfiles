5bcf71f73966d7c1de64a3cfc0a6cbc1ca3f4686
Devan Fischer <devan@devanfischer.com>
2021-11-02 20:00:47 -0700

    big changes coming

--- stow/zsh/.p10k.zsh
+++ stow/zsh/.p10k.zsh
@@ -98,14 +98,14 @@
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    sunMoon
+    # sunMoon
     # =========================[ Line #2 ]=========================
     newline
-    time                    # current time
+    # time                    # current time
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-    # battery               # internal battery
+    battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
