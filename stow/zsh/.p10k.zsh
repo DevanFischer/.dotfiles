4e00eeef3cdb211d9387ab9d64c157ecf604c4af
Devan Fischer <contact@devanfischer.com>
2022-10-24 14:57:40 -0700

    chore: remove battery from p10k

--- stow/zsh/.p10k.zsh
+++ stow/zsh/.p10k.zsh
@@ -100,12 +100,12 @@
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     # sunMoon
     # =========================[ Line #2 ]=========================
-    newline
+    # newline
     # time                    # current time
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-    battery               # internal battery
+    # battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
